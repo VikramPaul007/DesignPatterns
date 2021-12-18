8790c2752c80c90a9484b00558a787e5553a8296
Vikram Paul <vikrampaul1996@gmail.com>
2021-12-18 22:45:47 +0530

    Commit includes 1. Singleton design pattern implementation.

---
+++ DesignPatterns.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.Console;
+
+Console.WriteLine("Hello, World!");
+
+#region Test Singleton
+Console.WriteLine("\nTesting Singleton Design Pattern: Start\n");
+// Synchronous execution
+Singleton singleton = Singleton.Instance;
+singleton.WriteToConsole("Object created inside User class.");
+
+Singleton singleton2 = Singleton.Instance;
+singleton2.WriteToConsole("Object created inside Product class.");
+
+// Parallel invoke to check for multi-threaded access
+Parallel.Invoke(() =>
+{
+    Singleton singleton = Singleton.Instance;
+    singleton.WriteToConsole("Object created inside User class.");
+
+    Singleton singleton2 = Singleton.Instance;
+    singleton2.WriteToConsole("Object created inside Product class.");
+});
+
+// Lazy initialization
+Parallel.Invoke(() =>
+{
+    LazySingleton lazySingleton = LazySingleton.Instance;
+    lazySingleton.WriteToConsole("Object created inside User class.");
+
+    LazySingleton lazySingleton2 = LazySingleton.Instance;
+    lazySingleton2.WriteToConsole("Object created inside Product class.");
+});
+
+Console.WriteLine("\nTesting Singleton Design Pattern: End\n");
+#endregion
+
+Console.ReadLine();
---
+++ DesignPatterns.Console/Singleton/Singleton.cs
@@ -0,0 +1,80 @@
+﻿namespace DesignPatterns.Console
+{
+    // Sealed class to restrict inheritance
+    internal sealed class Singleton
+    {
+        // Counts the no of instances created of the class
+        private static int counter;
+
+        private static readonly object obj = new();
+
+        // Private constructor ensures that object is not
+        // initiated anywhere other than within the class itself
+        private Singleton()
+        {
+            counter++;
+            System.Console.WriteLine($"Total instance created of Singleton: {counter}");
+        }
+
+        // This static property returns the only instance created
+        private static Singleton? instance = null;
+
+        // Returns the only created instance of the class
+        public static Singleton Instance { 
+            get 
+            {
+                // Double-check locking
+                // As locking is an expensive operation,
+                // we will only use lock when the instance is null
+                if (instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+                return instance;
+            } 
+        }
+
+        public void WriteToConsole(string text)
+        {
+            System.Console.WriteLine(text);
+        }
+
+    }
+
+    // Lazy initialization of Singleton class using Lazy keyword
+    internal sealed class LazySingleton
+    {
+        // Counts the no of instances created of the class
+        private static int counter;
+
+        // Private constructor ensures that object is not
+        // initiated anywhere other than within the class itself
+        private LazySingleton()
+        {
+            counter++;
+            System.Console.WriteLine($"Total instance created of LazySingleton: {counter}");
+        }
+
+        // This static property returns the only instance created
+        private static Lazy<LazySingleton> instance = new Lazy<LazySingleton>(new LazySingleton());
+
+        // Returns the only created instance of the class
+        public static LazySingleton Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void WriteToConsole(string text)
+        {
+            System.Console.WriteLine(text);
+        }
+
+    }
+}
